3a4d4ecb8aef3fc4699bcdb5a1d8376ba5eb903f
Veselkova Irina <veselkovaiv@yandex.ru>
2024-06-13 20:32:00 +0300

    Add menu

--- OOP_Python_Program_Exam/Animal.py
+++ OOP_Python_Program_Exam/Animal.py
@@ -1,21 +1,29 @@
-from collections.abc import abc
+from abc import ABC, abstractmethod
 from datetime import date, datetime
+
 class Animal(ABC):
-    def __init__(self, name, type, birth_date, commands=[]) -> None:
+    
+    def __init__(self, name, type_animal, birth_date, commands=[]):
         self.name = name
-        self.type = type
+        self.type = type_animal
         data=birth_date.split('.') # введите дату в формате дд.мм.ГГГГ
         self.birth_date = date(int(data[2]),int(data[1]),int(data[0]))
         self.commands=list(commands)
+        self.reestr=Count()
+        
     @abstractmethod
     def __str__(self):
         year_age=(datetime.now()-self.birth_date).days()//365 
         #дописать возраст в днях, если разница меньше месяца
-        return f"Тип: {self.type}; кличка: {self.name}; возраст: {year_age} years; знает команды: {',  '.join(self.commands)}"
+        return f"Тип: {self.type}; кличка: {self.name}; возраст: {year_age} лет; знает команды: {',  '.join(self.commands)}"
+    
     def __add__(self, value):
-        pass
+        pass 
+    
     def __iadd__(self,value):
         pass
         
 
-
+class Count:
+    def __init__(self):
+        self.reestr_list=[]
